2a54a8e23eb819c2eadfffbf21438acd4d1b7e14
Tequilian <23119848+despokd@users.noreply.github.com>
2020-10-19 15:49:01 +0200

    Try to fix require

--- privacy/index.php
+++ privacy/index.php
@@ -9,7 +9,7 @@
 $page_title = 'Datenschutzerklärung';
 
 //insert HTML header
-require_once(dirname(__FILE__) . '/_includes/html_header.php');
+require_once '../_includes/html_header.php';
 ?>
 
 <main class="row">
@@ -96,4 +96,4 @@ require_once(dirname(__FILE__) . '/_includes/html_header.php');
 
 <?php
 //insert HTML footer
-require_once(dirname(__FILE__) . '/_includes/html_footer.php');
+require_once '../_includes/html_footer.php';
