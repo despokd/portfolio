6026441bf80e9aed8785b8b4b6f30a343f43c800
Tequilian <23119848+despokd@users.noreply.github.com>
2020-10-19 16:10:44 +0200

    Fix directory names

--- _includes/html_footer.php
+++ _includes/html_footer.php
@@ -12,7 +12,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
 <!-- Custom JS -->
-<script src="/assets/js/script.js"></script>
+<script src="/_assets/js/script.js"></script>
 </body>
 
 </html>
