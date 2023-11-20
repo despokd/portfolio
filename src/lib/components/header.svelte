364c58758642567e184ca90f22716bd80be251cd
Kilian Domaratius <github@kdomaratius.de>
2023-11-20 09:30:43 +0100

    show menu only on sub pages

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,11 +1,14 @@
 <script>
 	import { _ } from 'svelte-i18n';
+	import { page } from '$app/stores';
 </script>
 
-<header>
-	<nav>
-		<ul>
-			<li><a href="/">{$_('pages.home.title')}</a></li>
-		</ul>
-	</nav>
-</header>
+{#if $page.url.pathname != '/'}
+	<header>
+		<nav>
+			<ul>
+				<li><a href="/">{$_('pages.home.title')}</a></li>
+			</ul>
+		</nav>
+	</header>
+{/if}
