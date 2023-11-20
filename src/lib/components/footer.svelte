0c768dd7349136167f2c16933ace4292f685c981
Kilian Domaratius <github@kdomaratius.de>
2023-11-20 10:07:10 +0100

    lanaguage switch + footer basic styles

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,5 +1,13 @@
 <script>
-	import { _ } from 'svelte-i18n';
+	import { _, locale, locales } from 'svelte-i18n';
+
+	/**
+	 * Change current locale
+	 * @param {string | null | undefined} localeCode
+	 */
+	function setLocale(localeCode) {
+		locale.set(localeCode);
+	}
 </script>
 
 <footer>
@@ -11,6 +19,41 @@
 					>{$_('common.repository')}</a
 				>
 			</li>
+			{#each $locales as option}
+				<li>
+					<button
+						class:active={$locale === option}
+						class="a"
+						type="button"
+						on:click={() => setLocale(option)}
+						on:keydown={() => setLocale(option)}
+						on:touchend={() => setLocale(option)}
+						disabled={$locale === option}
+					>{option}</button>
+				</li>
+			{/each}
 		</ul>
 	</nav>
 </footer>
+
+<style lang="scss">
+	footer {
+		padding: 1rem;
+	}
+
+	ul {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+		gap: 1rem;
+	}
+
+	a, .a {
+		text-decoration: none;
+	}
+</style>
