6c903e9cbc56accace16b1fe8c3db91395d1f74a
Kilian Domaratius <github@kdomaratius.de>
2023-11-21 20:58:24 +0100

    add links

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -8,6 +8,9 @@
 		<nav>
 			<ul>
 				<li><a href="/">{$_('pages.home.title')}</a></li>
+				<li><a href="/snippets">{$_('pages.snippets.title')}</a></li>
+				<li><a href="/links">{$_('pages.links.title')}</a></li>
+				<li><a href="/projects">{$_('pages.projects.title')}</a></li>
 			</ul>
 		</nav>
 	</header>
---
+++ src/routes/links/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { _ } from 'svelte-i18n';
+</script>
+
+<svelte:head>
+	<title>{$_('pages.links.title')}</title>
+	<meta name="description" content={$_('pages.links.description')} />
+</svelte:head>
+
+<h1>{$_('pages.links.title')}</h1>
+<p>{$_('pages.links.description')}</p>
+
+<iframe
+	allowfullscreen
+	frameborder="0"
+	title={$_('pages.links.iframe')}
+	src="https://raindrop.io/despokd/tequilian-13645483/embed/sort=-created&theme=auto&hide=header"
+></iframe>
+
+<style lang="scss">
+	iframe {
+		width: 100%;
+		height: 500px;
+        min-height: 75vh;
+        margin-block: 2.5%;
+	}
+</style>
