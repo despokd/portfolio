c6fbf16d7674992f0480a34dd4f9ed964556235c
Kilian Domaratius <github@kdomaratius.de>
2023-11-24 20:51:43 +0100

    update raindrop link + add full screen link

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -1,5 +1,16 @@
 <script>
-    import { _ } from 'svelte-i18n';
+	import { _ } from 'svelte-i18n';
+
+	const raindropUrl = 'https://raindrop.io/despokd/shared-39428323';
+	const raindropOptions = {
+		sort: '-created',
+		theme: 'auto',
+		hide: 'header'
+	};
+
+	const raindropEmbedUrl = `${raindropUrl}/embed/${Object.entries(raindropOptions)
+		.map(([key, value]) => `${key}=${value}`)
+		.join('&')}`;
 </script>
 
 <svelte:head>
@@ -8,20 +19,19 @@
 </svelte:head>
 
 <h1>{$_('pages.links.title')}</h1>
-<p>{$_('pages.links.description')}</p>
+<p>
+	{$_('pages.links.description')}.
+	<a href={raindropUrl} target="_blank" rel="noopener noreferrer">{$_('pages.links.showCollection')}</a>
+</p>
 
-<iframe
-	allowfullscreen
-	frameborder="0"
-	title={$_('pages.links.iframe')}
-	src="https://raindrop.io/despokd/tequilian-13645483/embed/sort=-created&theme=auto&hide=header"
+<iframe allowfullscreen frameborder="0" title={$_('pages.links.iframe')} src={raindropEmbedUrl}
 ></iframe>
 
 <style lang="scss">
 	iframe {
 		width: 100%;
 		height: 500px;
-        min-height: 75vh;
-        margin-block: 2.5%;
+		min-height: 75vh;
+		margin-block: 2.5%;
 	}
 </style>
