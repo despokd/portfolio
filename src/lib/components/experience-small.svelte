a09415f9160f4d6b0c0dd521e76de8755fac5249
Kilian Domaratius <github@kdomaratius.de>
2023-11-29 14:55:31 +0100

    format

--- src/lib/components/experience-small.svelte
+++ src/lib/components/experience-small.svelte
@@ -1,14 +1,18 @@
 <script>
-	import { _, getDateFormatter } from 'svelte-i18n';
-    import { getTranslation } from '$lib/directus';
+	import { _ } from 'svelte-i18n';
+	import { getTranslation } from '$lib/directus';
 
 	export let experience;
 
-    const dateFormat = 'MMMM yyyy'
-    const title = experience.type == 'education' ? $_('pages.cv.experience.education') : $_('pages.cv.experience.job');
-    const start = experience.start ? getDateFormatter(dateFormat).format(new Date(experience.start)) : null;
-    const end = experience.end ? new Date(experience.end).getFullYear() : $_('pages.cv.experience.present');
-    const place = getTranslation(experience.translations).place ?? null;
+	const title =
+		experience.type == 'education'
+			? $_('pages.cv.experience.education')
+			: $_('pages.cv.experience.job');
+	const start = experience.start ? new Date(experience.start).getFullYear() : null;
+	const end = experience.end
+		? new Date(experience.end).getFullYear()
+		: $_('pages.cv.experience.present');
+	const place = getTranslation(experience.translations).place ?? null;
 </script>
 
 <div>{title}</div>
@@ -17,22 +21,22 @@
 	{#if place}
 		<li>{place}</li>
 	{/if}
-    <li>{start ?? ''}{end ? ` - ${end}` : ''}</li>
+	<li>{start ?? ''}{end ? ` - ${end}` : ''}</li>
 </ul>
 
 <style lang="scss">
-    ul {
-        list-style: none;
-        padding: 0;
-        margin: 0;
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        gap: .5em;
-    }
+	ul {
+		list-style: none;
+		padding: 0;
+		margin: 0;
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 0.5em;
+	}
 
-    h3 {
-        margin-top: 0;
-        margin-bottom: .25em;
-    }
+	h3 {
+		margin-top: 0;
+		margin-bottom: 0.25em;
+	}
 </style>
