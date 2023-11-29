70ebb3a14b5ff14dad9d99014dc12373b0e93d60
Kilian Domaratius <github@kdomaratius.de>
2023-11-29 21:16:09 +0100

    style header simple

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -3,16 +3,31 @@
 	import { page } from '$app/stores';
 </script>
 
-{#if $page.url.pathname != '/'}
-	<header>
-		<nav>
-			<ul>
-				<li><a href="/">{$_('pages.home.title')}</a></li>
-				<li><a href="/snippets">{$_('pages.snippets.title')}</a></li>
-				<li><a href="/links">{$_('pages.links.title')}</a></li>
-				<li><a href="/projects">{$_('pages.projects.title')}</a></li>
-				<li><a href="/cv">{$_('pages.cv.title')}</a></li>
-			</ul>
-		</nav>
-	</header>
-{/if}
+<header class:home={$page.url.pathname === '/'}>
+	<nav>
+		<ul>
+			<li><a href="/">{$_('pages.home.title')}</a></li>
+			<li><a href="/snippets">{$_('pages.snippets.title')}</a></li>
+			<li><a href="/links">{$_('pages.links.title')}</a></li>
+			<li><a href="/projects">{$_('pages.projects.title')}</a></li>
+			<li><a href="/cv">{$_('pages.cv.title')}</a></li>
+		</ul>
+	</nav>
+</header>
+
+<style lang="scss">
+	ul {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		align-items: center;
+		gap: 1em;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	li {
+		margin: 0;
+	}
+</style>
